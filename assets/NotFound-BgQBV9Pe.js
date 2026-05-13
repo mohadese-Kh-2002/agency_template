0b0b0df7844ab0639641333ede3dc3ad12d483aa
mohadese <mohadesekhedri1381@gmail.com>
2026-05-14 02:42:56 +0330

    Updates

--- assets/NotFound-BgQBV9Pe.js
+++ assets/NotFound-BgQBV9Pe.js
@@ -1,4 +1,4 @@
-import{j as e,L as r,k as n}from"./index-BvklS3Dc.js";const s=()=>e.jsx("div",{className:"min-h-screen flex items-center justify-center bg-(--bg) px-4",children:e.jsxs("div",{className:`\r
+import{j as e,L as r,k as n}from"./index-DmS_HsH7.js";const s=()=>e.jsx("div",{className:"min-h-screen flex items-center justify-center bg-(--bg) px-4",children:e.jsxs("div",{className:`\r
           relative\r
           w-full max-w-lg\r
           rounded-3xl\r
